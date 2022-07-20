89018e485f7f96db00dfb615f22a64003e760f50
Kohlten Bills <kohltenbillsy@gmail.com>
2022-07-20 14:38:33 -0600

    working through

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -3,7 +3,21 @@ const Account = require('./accounts-model')
 
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
-
+  const errMessage = { status: 400, }
+  const { name, budget } = req.body
+  if (name === undefined || budget === undefined) {
+    errMessage.message = 'name and budget are required'
+    next(errMessage)
+  } else if (typeof name !== 'string') {
+    errMessage.message = 'name of account must be a string'
+    next(errMessage)
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    errMessage.message = 'name of account must be between 3 and 100'
+    next(errMessage)
+  } else if (typeof budget!== 'number' || isNaN(budget)) {
+    errMessage.message = 'budget of account must be a number'
+    next(errMessage)
+  }
   next()
 }
 
