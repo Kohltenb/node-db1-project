a9b364f5e94a5dc5fd160740b3d1bbecb1b68d94
Kohlten Bills <kohltenbillsy@gmail.com>
2022-07-20 14:41:56 -0600

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -7,18 +7,20 @@ exports.checkAccountPayload = (req, res, next) => {
   const { name, budget } = req.body
   if (name === undefined || budget === undefined) {
     errMessage.message = 'name and budget are required'
-    next(errMessage)
   } else if (typeof name !== 'string') {
     errMessage.message = 'name of account must be a string'
-    next(errMessage)
   } else if (name.trim().length < 3 || name.trim().length > 100) {
     errMessage.message = 'name of account must be between 3 and 100'
-    next(errMessage)
   } else if (typeof budget!== 'number' || isNaN(budget)) {
     errMessage.message = 'budget of account must be a number'
-    next(errMessage)
+  } else if (budget > 0 || budget > 1000000) {
+    errMessage.message = 'budget of account is too large or too small'
   }
+  if (errMessage.message) {
+    next(errMessage)
+  } else {
   next()
+  }
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
